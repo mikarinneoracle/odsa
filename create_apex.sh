78349b9508ccfc10b4fccfc7764904fba46d1b13
Mika Rinne <mika.rinne@oracle.com>
2022-11-26 08:23:33 +0100

    ci/cd

--- create_apex.sh
+++ create_apex.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-export region=uk-london-1
-export dbname=pricing
-export conn=pricing_high
-export pwd=WelcomeFolks123#!
-export schema=PRICEADMIN
-export wsname=PRICEADMIN
-export application_id=100
 
 printf "set cloudconfig ./network/admin/Wallet.zip\nconn admin/${pwd}@${conn}\n/\n" > upd_apex.sql
 printf "begin\n" >> upd_apex.sql
--- script.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ./network/admin
-mv /tmp/Wallet.zip ./network/admin/
-wget https://objectstorage.eu-amsterdam-1.oraclecloud.com/p/BydeV7ct283Y4F2S9PECPEyeNI-U3xoaghdLQ0EuzNUEDIMoieyqu5uA7xJ-syyq/n/frsxwtjslf35/b/oracledb/o/jdk-11.0.16_linux-x64_bin.tar.gz -q
-tar -xzf jdk-11.0.16_linux-x64_bin.tar.gz
-export PATH=./jdk-11.0.16/bin:$PATH
-export JAVA_HOME=./jdk-11.0.16
-wget https://objectstorage.eu-amsterdam-1.oraclecloud.com/p/rBz7NIZQsEXsXN6yqnLn8m9fLmTHZUY7Z5uhPBBUzsoiW0ceUoY1jyU5y7DjEWJx/n/frsxwtjslf35/b/oracledb/o/V1022102-01.zip -q
-unzip -q V1022102-01.zip
-sh create_apex.sh
